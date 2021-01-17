e2636277d293d2b3e1d3bda80e696e3b0427f45b
TheGooodDev <72625343+TheGooodDev@users.noreply.github.com>
2021-01-17 16:42:44 +0100

    Merge Déplacement

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,37 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
-    private Vector3 translationH;
-    private Vector3 translationV;
-    public string up;
-    public string down;
-    public string left;
-    public string right;
     public Warrior warrior;
+    public Getinfo info;
+    private Vector3 pos;
+    public string player;
+    float xpx;
+    float ypy;
+    private bool diagonal = false;
 
-    void Start () {
-        translationH.y = 54f;
-        translationV.x = 54f;
-    }
+        void Start()
+        {
+        }
 
     void Update () {
         if (warrior && warrior.phase == "my turn") {
-            if (Input.GetKey (up)) {
-                transform.Translate (translationH);
-                warrior.phase = "end phase";
-            }
-            if (Input.GetKey (down)) {
-                transform.Translate (-translationH);
-                warrior.phase = "end phase";
-            }
-            if (Input.GetKey (right)) {
-                transform.Translate (translationV);
-                warrior.phase = "end phase";
-            }
-            if (Input.GetKey (left)) {
-                transform.Translate (-translationV);
-                warrior.phase = "end phase";
-            }
+                info = GameObject.Find("Main Camera").GetComponent<Getinfo>();
+                if (info.isground==true){
+                    float xp = GameObject.Find(warrior.name).transform.position.x;
+                    float yp = GameObject.Find(warrior.name).transform.position.y;
+                    Debug.Log(info.x + " " + info.y + " / " + xp + " " + yp );
+                    xpx = xp - info.x;
+                    ypy = yp - info.y;
+
+                    float z = -2f;
+                    if(xpx > -60 && xpx < 60){
+                        if(ypy > -60 && ypy < 60){
+                            if(((ypy > -60 && ypy < -50)||(ypy >50 && ypy <60))&&((xpx > -60 && xpx < -50)||(xpx >50 && xpx <60))){
+                                diagonal = true;
+                            }
+                            if(diagonal==false){
+                                Debug.Log("info.x: " + xpx+ " info y: " + ypy);
+                                transform.position = new Vector3 (info.x,info.y,z);
+                                warrior.phase = "end phase";
+                                info.isground=false;
+                            }
+                            diagonal = false;
+                        }
+                    }
+                }
         }
     }
 }
